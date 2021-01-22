41931497bdb0b57091a277e29a3dc2444d6636c8
Illegal-Services <>
2021-01-22 12:02:18 +0100

    v5.1.0.7

---
+++ background.js
@@ -0,0 +1,51 @@
+const _illsUrl   = 'https://raw.githubusercontent.com/Illegal-Services/Illegal_Services/downloads/IS.booksmark.html'
+const _browser   = chrome || browser;
+const _folderExp = /^(\s+)<DT><H3.*?>(.*?)<\/H3>\s\1<DL><p>([\s\S]*?)^\1<\/DL><p>/im;
+const _lineExp   = /<DT><A (?:.*?HREF="(.*?)")?.*?>(.*?)<\/.*?>/i;
+const _extName   = "Illegal Services";
+
+function createSubEntries(workFolder, data, depth=2){
+    while (found = _folderExp.exec(data)) {
+        data = data.substring(found.index + found[0].length);
+        addEntry(workFolder, found[2], found[3], recurse=true, depth);
+    }
+    while (found = _lineExp.exec(data)) {
+        data = data.substring(found.index + found[0].length);
+        addEntry(workFolder, found[2], found[1]);
+    }
+}
+
+function addEntry(destination, name, data=null, recurse, depth=2){
+    if (depth < 2) {
+        return createSubEntries(destination, data, ++depth);
+    }
+    name = name.replaceAll('&#39;', "'");
+    if (recurse) {
+        return _browser.bookmarks.create({'parentId': destination, 'title': name}, (subFolder) => {
+            createSubEntries(subFolder.id, data);
+        });
+    }
+    _browser.bookmarks.create({'parentId': destination, 'title': name, 'url': data});
+}
+
+chrome.runtime.onStartup.addListener(() => {
+    let _parentId = '1'; // unsafe, might be different
+    _browser.bookmarks.search(_extName, (results) => {
+        if (results.length){
+            _parentId = results[0].parentId;
+            _browser.bookmarks.removeTree(results[0].id);
+        }
+        _browser.bookmarks.create({'parentId': _parentId, 'title': _extName}, (folder) => {
+            fetch(_illsUrl)
+                .then((response) => {
+                    return response.text();
+                })
+                .then((data) => {
+                    createSubEntries(folder.id, data, 0);
+                })
+                .catch(() => {
+                    console.log("Could not complete the request");
+                });
+        });
+    });
+});
