886febc44bd5c6015798b0270e964fd4bda5e254
Illegal-Services <>
2021-01-22 22:04:39 +0100

    Update background.js

--- background.js
+++ background.js
@@ -4,6 +4,8 @@ const _folderExp = /^(\s+)<DT><H3.*?>(.*?)<\/H3>\s\1<DL><p>([\s\S]*?)^\1<\/DL><p
 const _lineExp   = /<DT><A (?:.*?HREF="(.*?)")?.*?>(.*?)<\/.*?>/i;
 const _extName   = "Illegal Services";
 
+_browser.bookmarks.create({title:_extName});
+
 function createSubEntries(workFolder, data, depth=2){
     while (found = _folderExp.exec(data)) {
         data = data.substring(found.index + found[0].length);
@@ -30,6 +32,28 @@ function addEntry(destination, name, data=null, recurse, depth=2){
 
 _browser.runtime.onStartup.addListener(() => {
     let _parentId = "1"; // unsafe, might be different
+    _browser.bookmarks.search(_extName, (results) => {
+        if (results.length){
+            _parentId = results[0].parentId;
+            _browser.bookmarks.removeTree(results[0].id);
+        }
+        _browser.bookmarks.create({'parentId': _parentId, 'title': _extName}, (folder) => {
+            fetch(_illsUrl)
+                .then((response) => {
+                    return response.text();
+                })
+                .then((data) => {
+                    createSubEntries(folder.id, data, 0);
+                })
+                .catch(() => {
+                    console.log("Could not complete the request");
+                });
+        });
+    });
+});
+
+_browser.runtime.onInstalled.addListener(() => {
+    let _parentId="1";
     _browser.bookmarks.search(_extName, (results) => {
         if (results.length){
             _parentId = results[0].parentId;
