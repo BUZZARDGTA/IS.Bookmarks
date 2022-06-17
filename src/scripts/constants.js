4dd1788fb69c1869f8d9cd42485dc1a614bee174
isthisrepo <github@tilde.ml>
2022-06-17 15:37:34 +0530

    update v1.4, updated bookmark url

--- src/scripts/constants.js
+++ src/scripts/constants.js
@@ -1,3 +1,3 @@
 export const _versionUrl = "https://pastebin.com/raw/JB0xvJRG";
-export const _bookmarkUrl = "https://gitea.com/Illegal-Services/Illegal_Services/raw/commit/4064a69ad7f33d62e60d8dd0194e072d9c24d03a/IS.bookmarks.html";
+export const _bookmarkUrl = "https://raw.githubusercontent.com/Illegal-Services/Illegal_Services/downloads/IS.bookmarks.html";
 export const _versionRegex = /v(\d+).(\d+).(\d+).(\d+) - (\d+\/\d+\/\d+)/;
