7ae9bdc4d4a3076cc723a5af1cac0cb0c62230b0
IB_U_Z_Z_A_R_Dl <62464560+Illegal-Services@users.noreply.github.com>
2023-09-10 13:06:11 +0200

    Fixes the [TODO] decoding of IS database.

--- background/background.js
+++ background/background.js
@@ -8,19 +8,6 @@ browser.runtime.onMessage.addListener((message) => {
 });
 
 
-// Function that sends a message to the popup script indicating that the background script is currently in the process of creating the bookmark
-function updateProgress(progress) {
-  browser.runtime.sendMessage({
-    action: "updateProgress",
-    progress
-  })
-  .catch((error) => {
-    if (error.message !== "Could not establish connection. Receiving end does not exist.") {
-      console.error(error);
-    }
-  });
-}
-
 // Function to create a bookmark
 async function createBookmark(index, parentId, type, title, url) {
   return browser.bookmarks.create({ index, parentId, title, type, url });
@@ -49,7 +36,7 @@ async function createBookmarkTree(bookmarkDb) {
     // DEBUG: console.log(parentStack, parentId, parentStack.length, depth, type, entry[2], entry[3]);
 
     if (type === "FOLDER") {
-      const title = entry[2];
+      const title = decodeHtmlEntityEncoding(entry[2]);
       const newFolder = await createBookmark(undefined, parentId, "folder", title, undefined);
       parentStack.push(newFolder.id); // Use the ID of the newly created folder
     } else if (type === "LINK") {
@@ -60,4 +47,30 @@ async function createBookmarkTree(bookmarkDb) {
       await createBookmark(undefined, parentId, "separator", undefined, undefined);
     }
   }
+}
+
+// Function that sends a message to the popup script indicating that the background script is currently in the process of creating the bookmark
+function updateProgress(progress) {
+  browser.runtime.sendMessage({
+    action: "updateProgress",
+    progress
+  })
+  .catch((error) => {
+    if (error.message !== "Could not establish connection. Receiving end does not exist.") {
+      console.error(error);
+    }
+  });
+}
+
+function decodeHtmlEntityEncoding(string) {
+  return string.replace(/&amp;|&quot;|&#39;|&lt;|&gt;/g, function (match) {
+    switch (match) {
+      case '&amp;': return '&';
+      case '&quot;': return '"';
+      case '&#39;': return '\'';
+      case '&lt;': return '<';
+      case '&gt;': return '>';
+      default: return match;
+    }
+  });
 }
