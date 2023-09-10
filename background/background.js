c9aca1aba01e7082b566dd9aae3b5c467619b049
IB_U_Z_Z_A_R_Dl <62464560+Illegal-Services@users.noreply.github.com>
2023-09-10 14:58:15 +0200

    Update background.js

--- background/background.js
+++ background/background.js
@@ -1,6 +1,5 @@
 // Add an event listener for the 'onInstalled' event, which means it will run when the extension when it will be first installed
 browser.runtime.onInstalled.addListener((details) => {
-  console.log(details);
   if (details.reason === "install") {
     return initializeCreationOfBookmarkTree();
   }
@@ -10,6 +9,7 @@ browser.runtime.onInstalled.addListener((details) => {
 
 // Add an event listener for the 'onStartup' event, which means it will run when Firefox starts up or when a new browser window is opened
 browser.runtime.onStartup.addListener((details) => {
+  // Here I couldn't find a way to check (if details.reason === "startup") so, this lines will be changed or not in release v1.6
   console.log(details);
   if (details.reason === "startup") {
     return initializeCreationOfBookmarkTree();
