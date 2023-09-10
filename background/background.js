bcd25e0934a95d49c7e64669c761b9f4694e1584
IB_U_Z_Z_A_R_Dl <62464560+Illegal-Services@users.noreply.github.com>
2023-09-10 11:36:47 +0200

    initial release

---
+++ background/background.js
@@ -0,0 +1,63 @@
+// Listen for incoming messages from the extension's UI "Reload" button pressed
+browser.runtime.onMessage.addListener((message) => {
+  if (message.action !== "createBookmarksTree") {
+    return false;
+  }
+
+  return createBookmarkTree(message.bookmarkDb);
+});
+
+
+// Function that sends a message to the popup script indicating that the background script is currently in the process of creating the bookmark
+function updateProgress(progress) {
+  browser.runtime.sendMessage({
+    action: "updateProgress",
+    progress
+  })
+  .catch((error) => {
+    if (error.message !== "Could not establish connection. Receiving end does not exist.") {
+      console.error(error);
+    }
+  });
+}
+
+// Function to create a bookmark
+async function createBookmark(index, parentId, type, title, url) {
+  return browser.bookmarks.create({ index, parentId, title, type, url });
+}
+
+// Function to create a bookmark tree
+async function createBookmarkTree(bookmarkDb) {
+  const parentStack = ["toolbar_____"]; // Start with the Bookmarks Toolbar as the initial parent
+
+  const total = bookmarkDb.length - 1; // Removes -1 because 'index' starts from 0
+  const enumeratedDb = bookmarkDb.map((value, index) => [index, value]);
+  for (const [index, entry] of enumeratedDb) {
+    updateProgress(index * 100 / total);
+
+    const type = entry[0];
+    const depth = parseInt(entry[1]);
+
+    const depthToRemove = (parentStack.length - depth);
+
+    if (depthToRemove > 0) {
+      parentStack.splice(-depthToRemove);
+    }
+
+    const parentId = parentStack[parentStack.length - 1];
+
+    // DEBUG: console.log(parentStack, parentId, parentStack.length, depth, type, entry[2], entry[3]);
+
+    if (type === "FOLDER") {
+      const title = entry[2];
+      const newFolder = await createBookmark(undefined, parentId, "folder", title, undefined);
+      parentStack.push(newFolder.id); // Use the ID of the newly created folder
+    } else if (type === "LINK") {
+      const url = entry[2];
+      const title = entry[3];
+      await createBookmark(undefined, parentId, "bookmark", title, url);
+    } else if (type === "HR") {
+      await createBookmark(undefined, parentId, "separator", undefined, undefined);
+    }
+  }
+}
---
+++ popup/popup.js
@@ -0,0 +1,140 @@
+document.addEventListener("DOMContentLoaded", async function () {
+  const htmlISDbDate = document.getElementById("ISDbDate");
+  const htmlReloadButton = document.getElementById("reloadButton");
+
+  browser.runtime.onMessage.addListener(messageListener);
+
+  // Add a click event listener to the 'htmlReloadButton' button
+  htmlReloadButton.addEventListener("click", reload);
+
+  const urlISDbDate = "https://api.github.com/repos/Illegal-Services/IS.Bookmarks/commits?path=IS.bookmarks.json&sha=extra&per_page=1";
+  const urlRawISDatabase = "https://raw.githubusercontent.com/Illegal-Services/IS.Bookmarks/extra/IS.bookmarks.json";
+
+  fetch(urlISDbDate)
+  .then(response => response.json())
+  .then(data => {
+    const commitDate = data[0].commit.committer.date;
+
+    const options = {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "numeric",
+      minute: "2-digit",
+      hour12: true,
+    };
+
+    const formattedDate = new Intl.DateTimeFormat("en-US", options).format(new Date(commitDate));
+    htmlISDbDate.innerText = formattedDate;
+  })
+  .catch(error => console.error("Error:", error));
+
+  async function searchBookmarksWithTypeAndDepth(query, url, title, type, depth) {
+    // Search for bookmarks with the specified title
+    const bookmarks = await browser.bookmarks.search({ query, url, title });
+
+    // Function to calculate the depth of a bookmark using async recursion
+    function calculateDepth(nodeId, currentDepth) {
+      if (currentDepth > depth) {
+        return -1; // Mark bookmarks beyond the specified depth
+      }
+
+      return browser.bookmarks.get(nodeId).then((node) => {
+        if (!node.parentId) {
+          return currentDepth; // We've reached the root node
+        }
+        return calculateDepth(node.parentId, currentDepth + 1);
+      });
+    }
+
+    // Filter the bookmarks to include only those at the specified depth and match the target type
+    const filteredBookmarks = await Promise.all(
+      bookmarks.map(async (bookmark) => {
+        const bookmarkDepth = await calculateDepth(bookmark.id, 0);
+        if (bookmarkDepth === depth && bookmark.type === type) {
+          return bookmark;
+        }
+        return null;
+      })
+    );
+
+    // Remove null values (bookmarks that didn't match the criteria)
+    const validBookmarks = filteredBookmarks.filter((bookmark) => bookmark !== null);
+
+    return validBookmarks;
+  }
+
+  async function reload() {
+    // Remove previous "Illegal Services" bookmark folder(s) from depth 0
+    const filteredBookmarks = await searchBookmarksWithTypeAndDepth(null, null, "Illegal Services", "folder", 0);
+    for (const object of filteredBookmarks) {
+      await browser.bookmarks.removeTree(object.id);
+    }
+
+    htmlReloadButton.className = "btn btn-secondary w-50 disabled";
+
+    let _response;
+    try {
+      _response = await fetch(urlRawISDatabase);
+    } catch (error) {
+      console.error(error);
+    }
+    const response = _response;
+
+    if (
+      (response === undefined)
+      || (!response.ok)
+      || (response.status !== 200)
+    ) {
+      htmlReloadButton.innerText = "FAIL";
+      htmlReloadButton.className = "btn btn-danger w-50 disabled";
+      return;
+    }
+
+    let responseText = await response.text();
+    responseText = responseText.trim();
+
+    let _bookmarkDb;
+    try {
+      _bookmarkDb = JSON.parse(responseText);
+    } catch (error) {
+      console.error(error);
+    }
+    let bookmarkDb = _bookmarkDb;
+
+    if (
+      (bookmarkDb === undefined)
+      || (!Array.isArray(bookmarkDb))
+      || (JSON.stringify(bookmarkDb[0]) !== '["FOLDER",0,"Bookmarks Toolbar"]') // Checks if the first array from the 'bookmarkDb' correctly matches the official IS bookmarks database
+    ) {
+      htmlReloadButton.innerText = "FAIL";
+      htmlReloadButton.className = "btn btn-danger w-50 disabled";
+      return;
+    }
+
+    bookmarkDb = bookmarkDb.slice(1); // Slice the very first array which contains the "Bookmarks Toolbar" folder
+
+    // Send a message to the extension's background script to initiate the closing of tabs
+    await browser.runtime.sendMessage({
+      action: "createBookmarksTree",
+      bookmarkDb
+    });
+  }
+
+  function messageListener(message) {
+    if (message.action !== "updateProgress") {
+      return false;
+    }
+
+    if (message.progress === 100) {
+      htmlReloadButton.innerText = "DONE";
+      htmlReloadButton.className = "btn btn-success w-50";
+    } else {
+      if (htmlReloadButton.className !== "btn btn-secondary w-50 disabled") {
+        htmlReloadButton.className = "btn btn-secondary w-50 disabled";
+      }
+
+      htmlReloadButton.innerText = `${message.progress.toFixed(1)}%`;
+    }
+  }
+});
